182b005fcbbc512ef8b843e9bcfd92cc684fd70f
Erica_ntsc <88816192+Entiesci@users.noreply.github.com>
2024-11-18 09:02:23 +0800

    Update main.dart

--- dart_web_application_1/web/main.dart
+++ dart_web_application_1/web/main.dart
@@ -20,13 +20,13 @@ web.HTMLLIElement newLI(String itemText) =>
  
 
 void main() {
-  // var now = DateTime.now();
-  // var element = web.document.querySelector('#output') as web.HTMLDivElement;
-  // element.text = 'The time is ${now.hour}:${now.minute} '
-  //     'and your Dart web app is running!';
+  var now = DateTime.now();
+  var element = web.document.querySelector('#output') as web.HTMLDivElement;
+  element.text = 'The time is ${now.hour}:${now.minute} '
+      'and your Dart web app is running!';
 
   final output = web.document.querySelector('#output');
- for (final item in thingsTodo()) {
-   output?.appendChild(newLI(item));
- }
+  for (final item in thingsTodo()) {
+    output?.appendChild(newLI(item));
+  }
 }
